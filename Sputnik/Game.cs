07bf10b6f6754452092d0484343b3ccc35440f9b
Nicola Page <nicolapage@protonmail.com>
2024-10-13 12:30:16 +0100

    allow cancelling out of game

--- Sputnik/Game.cs
+++ Sputnik/Game.cs
@@ -4,27 +4,55 @@
     {
         public static List<IMiniGame> MiniGames { get; set; }
 
-        public static async Task Play()
+        private static bool awaitingGameChoice;
+        public static async Task Play(CancellationToken token)
         {
-            MiniGames = [new NumberCrunch(), new WordHunt()];
-
-            Console.WriteLine("Select your gameChoice");
-            for (int i = 0; i < MiniGames.Count; i++)
+            while (!token.IsCancellationRequested)
             {
-                Console.WriteLine($"{i + 1}: {MiniGames[i]}");
+                MiniGames = [new NumberCrunch(), new WordHunt()];
+
+                Console.WriteLine("Select your game");
+                for (int i = 0; i < MiniGames.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}: {MiniGames[i]}");
+                }
+
+                awaitingGameChoice = true;
+                while (awaitingGameChoice && !token.IsCancellationRequested)
+                {
+                    var gameChoice = await Task.Run(() => ListenForInput(token), token);
+                    if (!token.IsCancellationRequested)
+                    {
+                        if (gameChoice != null && (int)gameChoice > 0 && (int)gameChoice <= MiniGames.Count)
+                        {
+                            awaitingGameChoice = false;
+                            var game = MiniGames[(int)gameChoice - 1];
+                            await game.Play();
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Please select a game between 1 and {MiniGames.Count}");
+                        };
+                    }
+                }
             }
 
-            if (int.TryParse(Console.ReadLine(), out var gameChoice)
-                && gameChoice > 0 && gameChoice <= MiniGames.Count)
-            {
-                var game = MiniGames[gameChoice - 1];
-                await game.Play();
-            }
-            else
-            {
-                Console.WriteLine($"Please select a game between 1 and {MiniGames.Count}");
-            };
+        }
 
+        private static async Task<int?> ListenForInput(CancellationToken token)
+        {
+            
+            while (!token.IsCancellationRequested)
+            {
+                if(Console.KeyAvailable)
+                {
+                    var input = Console.ReadLine();
+                    if (int.TryParse(input, out var gameChoice))
+                        return gameChoice;
+                }
+                
+            }
+            throw new OperationCanceledException();
         }
     }
 }
--- Sputnik/Program.cs
+++ Sputnik/Program.cs
@@ -2,9 +2,31 @@
 {
     internal class Program
     {
+        private static CancellationTokenSource _cts = new();
+
         static async Task Main(string[] args)
         {
-            await Game.Play();
+            Console.CancelKeyPress += Console_CancelKeyPress;
+
+            try
+            {
+                await Game.Play(_cts.Token);
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Cancelled");
+            }
+            finally
+            {
+                Console.WriteLine("Exiting");
+            }
+            
+        }
+
+        private static void Console_CancelKeyPress(object? sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            _cts.Cancel();
         }
     }
 }
