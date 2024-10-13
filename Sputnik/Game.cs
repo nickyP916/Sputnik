0734a62eb9b501a27242f656040821469bbcae08
Nicola Page <nicolapage@protonmail.com>
2024-10-13 10:43:26 +0100

    Word Hund added and ability to choose game'

--- Sputnik/Game.cs
+++ Sputnik/Game.cs
@@ -6,8 +6,25 @@
 
         public static async Task Play()
         {
-            var _numberCrunch = new NumberCrunch();
-            await _numberCrunch.Play();
+            MiniGames = [new NumberCrunch(), new WordHunt()];
+
+            Console.WriteLine("Select your gameChoice");
+            for (int i = 0; i < MiniGames.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}: {MiniGames[i]}");
+            }
+
+            if (int.TryParse(Console.ReadLine(), out var gameChoice)
+                && gameChoice > 0 && gameChoice <= MiniGames.Count)
+            {
+                var game = MiniGames[gameChoice - 1];
+                await game.Play();
+            }
+            else
+            {
+                Console.WriteLine($"Please select a game between 1 and {MiniGames.Count}");
+            };
+
         }
     }
 }
---
+++ Sputnik/WordHunt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sputnik
+{
+    internal class WordHunt : IMiniGame
+    {
+        public string Name => "Word Hunt";
+
+        public async Task Play()
+        {
+            Console.WriteLine($"You are playing {Name}!");
+        }
+    }
+}
