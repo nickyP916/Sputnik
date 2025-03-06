e1e9df252875d20a74204144e892053ac9a5205e
Nicola Page <nicolapage@protonmail.com>
2025-03-06 14:57:29 +0000

    added console service

---
+++ Sputnik/Services/ConsoleService.cs
@@ -0,0 +1,24 @@
+﻿namespace Sputnik.Services
+{
+    public class ConsoleService : IConsoleService
+    {
+        public int CursorTop { get => Console.CursorTop; }
+        public int CursorLeft { get => Console.CursorLeft; }
+        public ConsoleColor ForegroundColor { set => Console.ForegroundColor = value; }
+
+        public void ResetColor()
+        {
+            Console.ResetColor();
+        }
+
+        public void SetCursorPosition(int left, int top)
+        {
+            Console.SetCursorPosition(left, top);
+        }
+
+        public void Write(char value)
+        {
+            Console.Write(value);
+        }
+    }
+}
---
+++ Sputnik/Services/IConsoleService.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Sputnik.Services
+{
+    public interface IConsoleService
+    {
+        int CursorTop { get; }
+        int CursorLeft { get; }
+        ConsoleColor ForegroundColor { set; }
+
+        void SetCursorPosition(int left, int top);
+        void Write(char value);
+        void ResetColor();
+    }
+}
