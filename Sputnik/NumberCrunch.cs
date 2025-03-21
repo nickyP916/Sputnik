a02d906bddeeec1cd58c312e879211a31ea858f8
Nicola Page <nicolapage@protonmail.com>
2025-03-21 12:03:14 +0000

    numbercrunch uses playmechanicsservice

--- Sputnik/NumberCrunch.cs
+++ Sputnik/NumberCrunch.cs
@@ -1,64 +1,88 @@
-﻿using Sputnik.Services;
+﻿using Newtonsoft.Json.Linq;
+using Sputnik.Services;
 
 namespace Sputnik
 {
-    internal class NumberCrunch(IInputListener inputListener) : IMiniGame
+    public record GameInstructions
+    {
+        public IInputListener InputListener { get; set; }
+        public Func<int,bool> ScoreLogic { get; set; }
+        public Action Setup { get; set; }
+    }
+
+    internal class NumberCrunch(IInputListener inputListener, IPlayMechanicsService playMechanics) : IMiniGame
     {
         public string Name => "Number Crunch";
-
-        private readonly int _maxRounds = 10;
 
         private int _roundsWon = 0;
 
         public async Task Play(CancellationToken token)
         {
-            Console.WriteLine($"You are playing {Name}!");
-
-            while (_roundsWon < _maxRounds && !token.IsCancellationRequested)
+            var instructions = new GameInstructions()
             {
-                var number1 = new Random().Next(100);
-                var number2 = new Random().Next(100);
+                InputListener = inputListener,
+                ScoreLogic = ScoreLogic,
+                Setup = Setup
+            };
+            await playMechanics.PlayToMaxRounds(instructions, token);
 
-                Console.WriteLine($"What is {number1} + {number2}?");
-
-                var cts = new CancellationTokenSource();
-                var combinedTcs = CancellationTokenSource.CreateLinkedTokenSource(token, cts.Token);
-                var inputTask = Task.Run(() => ListenForNumberInput(combinedTcs.Token));
-                if (await Task.WhenAny(inputTask, Task.Delay(5000)) == inputTask)
-                {
-                    if(!token.IsCancellationRequested)
-                    {
-                        if (inputTask.Result != null && number1 + number2 == (int)inputTask.Result)
-                        {
-                            Console.WriteLine("Correct!");
-                            _roundsWon++;
-                            continue;
-                        }
-                        Console.WriteLine("Wrong!");
-                        break;
-                    }
-                }
-                else
-                {
-                    cts.Cancel();
-                    var position = Console.CursorTop;
-                    Console.SetCursorPosition(0, position + 1);
-
-                    Console.WriteLine("Time's up!");
-                    break;
-                }
-            }
             var totalScore = RoundsScoreCalculator.CalculateScore(_roundsWon);
             Console.WriteLine($"Total Score: {totalScore}");
         }
 
-        private int? ListenForNumberInput(CancellationToken token)
+        private bool ScoreLogic(int guess)
         {
-            var input = inputListener.ListenForInput(token);
-            if (int.TryParse(input, out var answer))
-                return answer;
-            else
-                return null;
+            return true;
         }
+
+        private void Setup()
+        {
+            var number1 = new Random().Next(100);
+            var number2 = new Random().Next(100);
+
+            Console.WriteLine($"What is {number1} + {number2}?");
+        }
+
+        //public async Task Play(CancellationToken token)
+        //{
+        //    Console.WriteLine($"You are playing {Name}!");
+
+        //    while (_roundsWon < _maxRounds && !token.IsCancellationRequested)
+        //    {
+        //        var number1 = new Random().Next(100);
+        //        var number2 = new Random().Next(100);
+
+        //        Console.WriteLine($"What is {number1} + {number2}?");
+
+        //        var cts = new CancellationTokenSource();
+        //        var combinedTcs = CancellationTokenSource.CreateLinkedTokenSource(token, cts.Token);
+        //        var inputTask = Task.Run(() => inputListener.ListenForNumberInput(combinedTcs.Token));
+        //        if (await Task.WhenAny(inputTask, Task.Delay(5000)) == inputTask)
+        //        {
+        //            if (!token.IsCancellationRequested)
+        //            {
+        //                if (inputTask.Result != null && number1 + number2 == (int)inputTask.Result)
+        //                {
+        //                    Console.WriteLine("Correct!");
+        //                    _roundsWon++;
+        //                    continue;
+        //                }
+        //                Console.WriteLine("Wrong!");
+        //                break;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            cts.Cancel();
+        //            var position = Console.CursorTop;
+        //            Console.SetCursorPosition(0, position + 1);
+
+        //            Console.WriteLine("Time's up!");
+        //            break;
+        //        }
+        //    }
+        //    var totalScore = RoundsScoreCalculator.CalculateScore(_roundsWon);
+        //    Console.WriteLine($"Total Score: {totalScore}");
+        //}
     }
 }
